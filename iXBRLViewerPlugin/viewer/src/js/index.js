f0c3df98133eca72bcc15155cfb784ee2e256056
Paul Warren <pdw@ex-parrot.com>
2019-05-20 10:06:34 +0100

    Include babel polyfill.

--- iXBRLViewerPlugin/viewer/src/js/index.js
+++ iXBRLViewerPlugin/viewer/src/js/index.js
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import "babel-polyfill";
 import interact from 'interactjs'
 import $ from 'jquery'
 import { iXBRLReport } from "./report.js";
